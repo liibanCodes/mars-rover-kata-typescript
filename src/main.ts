2a23a90e3f272f255195363a459ccb8037cfce46
liibanCodes <liiban09@hotmail.co.uk>
2024-06-05 17:21:03 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+console.log("TEST TYPESCRIPT FILE");
+let age = 21;
+
+age > 20
+  ? console.log("IT is greater then 20")
+  : console.log("IT is less then 20");
+
+const names = [];
+
+for (let i; i < 10; i++) {}
+
+function getListOfNames() {}
+getListOfNames();
+
+const getNewList = () => {};
+
+// TODO: Right functions here
+const turnRight = () => {
+  console.log(
+    "(ONLY FOR EXAMPLE USE, not example of functionality): TURNING RIGHT",
+  );
+};
+
+const main = () => {
+  console.log("------------ MARS ROVER APP STARTING ------------");
+  // TODO: Right code to be run in here e.g.
+  turnRight();
+};
+
+main();
